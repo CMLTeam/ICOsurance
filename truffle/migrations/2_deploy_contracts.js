6f34239f08a1408017ef9c3c9e767917c48431a1
xonix <xonixx@gmail.com>
2017-07-23 02:21:57 +0300

    Crowdsale insurance code

--- truffle/migrations/2_deploy_contracts.js
+++ truffle/migrations/2_deploy_contracts.js
@@ -1,5 +1,11 @@
+var InsuranceToken = artifacts.require("./InsuranceToken.sol");
 var CoolICOToken = artifacts.require("./CoolICOToken.sol");
+var Crowdsale = artifacts.require("./Crowdsale.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(CoolICOToken);
+  deployer.deploy(InsuranceToken).then(function () {
+      deployer.deploy(CoolICOToken).then(function () {
+          deployer.deploy(Crowdsale, [10, 5, 2, InsuranceToken, CoolICOToken]);
+      });
+  });
 };
