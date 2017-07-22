5209358d98050cfa0cd184540e43e28c4f28fb33
vgorin <vgrn.at@gmail.com>
2017-07-22 19:41:03 +0300

    listen on 10.0.110.143

--- titan/join-titan.sh
+++ titan/join-titan.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-geth --datadir d/ --keystore k/ --networkid 20170125 console --rpc --rpccorsdomain "*"
+geth --datadir d/ --keystore k/ --networkid 20170125 console --rpc --rpccorsdomain "*" --mine --minerthreads 1 --rpcaddr 10.0.110.143
