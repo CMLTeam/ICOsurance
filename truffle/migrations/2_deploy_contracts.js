08805b8c704ee07c46a89a227f168624c3997c12
xonix <xonixx@gmail.com>
2017-07-22 19:51:33 +0300

    ERC20 token fix

--- truffle/migrations/2_deploy_contracts.js
+++ truffle/migrations/2_deploy_contracts.js
@@ -7,6 +7,5 @@ module.exports = function(deployer) {
   // deployer.deploy(ConvertLib);
   // deployer.link(ConvertLib, MetaCoin);
   // deployer.deploy(MetaCoin);
-  deployer.deploy(ERC20Interface);
   deployer.deploy(FixedSupplyToken);
 };
