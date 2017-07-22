18f0a6f543c3a9a0a793d255b1c3bd2684d38a8f
xonix <xonixx@gmail.com>
2017-07-22 19:48:50 +0300

    ERC20 token

--- truffle/migrations/2_deploy_contracts.js
+++ truffle/migrations/2_deploy_contracts.js
@@ -1,8 +1,12 @@
-var ConvertLib = artifacts.require("./ConvertLib.sol");
-var MetaCoin = artifacts.require("./MetaCoin.sol");
+// var ConvertLib = artifacts.require("./ConvertLib.sol");
+// var MetaCoin = artifacts.require("./MetaCoin.sol");
+var ERC20Interface = artifacts.require("./ERC20Interface.sol");
+var FixedSupplyToken = artifacts.require("./FixedSupplyToken.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
+  // deployer.deploy(ConvertLib);
+  // deployer.link(ConvertLib, MetaCoin);
+  // deployer.deploy(MetaCoin);
+  deployer.deploy(ERC20Interface);
+  deployer.deploy(FixedSupplyToken);
 };
