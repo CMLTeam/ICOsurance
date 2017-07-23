482389e727f1105d70ec3cc2e6fa9343cdfb57b9
xonix <xonixx@gmail.com>
2017-07-23 12:32:41 +0300

    make it compile

--- truffle/migrations/2_deploy_contracts.js
+++ truffle/migrations/2_deploy_contracts.js
@@ -4,8 +4,8 @@ var Crowdsale = artifacts.require("./Crowdsale.sol");
 
 module.exports = function(deployer) {
     deployer.deploy(CoolICOToken).then(function () {
-      deployer.deploy(InsuranceToken, "COOL").then(function () {
-          deployer.deploy(Crowdsale, 10, 5, 2, InsuranceToken, CoolICOToken);
-      });
-  });
+        deployer.deploy(InsuranceToken).then(function () {
+            deployer.deploy(Crowdsale, [10, 5, 2, InsuranceToken.address, CoolICOToken]);
+        });
+    })
 };
